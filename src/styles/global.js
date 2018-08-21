a15c68fcb4fbd5b6ac11fe5a5cd61b258b7a06f3
Vinícius Santos <vinishift@gmail.com>
2018-08-21 19:02:13 -0300

    css adjustments

--- src/styles/global.js
+++ src/styles/global.js
@@ -7,7 +7,6 @@ injectGlobal`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    outline: 0;
   }
 
   html,
@@ -22,6 +21,10 @@ injectGlobal`
     background: #FFF;
     font-family: Helvetica, Arial, sans-serif;
     color: #000;
+  }
+
+  input, button {
+    outline: 0;
   }
 
   button {
